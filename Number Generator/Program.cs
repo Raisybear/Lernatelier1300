5ccf64c56860b46413eed7dfd59d3d6842f8e5bd
Raisybear <elias.spycher@gmail.com>
2023-08-18 09:31:24 +0200

    d
    d

--- Number Generator/Program.cs
+++ Number Generator/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+internal class programm
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Zwischen welchen Zahlen sollte die Nummer generiert werden?");
+        int rangeNumber1 = Convert.ToInt32(Console.ReadLine());
+        int rangeNumber2 = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("es wird eine Zahl zwischen " + rangeNumber1 + " und " + rangeNumber2 + " generiert");
+
+        Random rnd = new Random();
+        int Geheimzahl = rnd.Next(1, 100);
+    }
+
+}
